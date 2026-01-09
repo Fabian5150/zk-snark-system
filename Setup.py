74d454cdd0dfdd3fd2c5272b27789e142f37c886
Fabian Strauch <fabian.strauch.04@web.de>
2026-01-09 17:14:44 +0100

    evaluate qap polynomials at tau, put psi linear combinations together and cleanup

--- Setup.py
+++ Setup.py
@@ -1,4 +1,4 @@
-from py_ecc.bn128 import G1, G2, pairing, add, multiply, eq, curve_order, neg
+from py_ecc.bn128 import G1, G2, multiply, curve_order
 import numpy as np
 import random
 import galois
@@ -8,11 +8,43 @@ class Setup:
     GF = galois.GF(curve_order)
 
     """
-    Returns a random interger between zero and the G1/G2-curve order (exclusive)
+    Takes the R1CS as a already interpolated QAP (2d array of coefficients)
+    to construct the trusted setup
+    Polynomials of QAP must be given row-wise,
+    with coefficients of higher powers first
     """
-    @staticmethod
-    def __get_random_scalar():
-        return random.randint(1, curve_order - 1) # Must not be 0
+    def __init__(
+        self,
+        out_poly: np.ndarray, # polynomials of the output side of the qap
+        left_poly: np.ndarray, # polynomials of the left factor of the qap
+        right_poly: np.ndarray, # polynomial of the right factor of the qap
+    ):
+        self.out_poly = out_poly
+        self.left_poly = left_poly
+        self.right_poly = right_poly
+        
+        ### to be kept private
+        # for powers of tau
+        self.tau = self.__get_random_scalar()
+        self.tau_GF = self.GF(self.tau)
+
+        # for multiplication with the QAP matrices
+        self.alpha = self.__get_random_scalar()
+        self.beta = self.__get_random_scalar()
+        ###
+
+        self.poly_degree = len(self.out_poly[0])
+
+        self.g1_srs = self.__get_srs(self.poly_degree + 1, G1)
+        self.g2_srs = self.__get_srs(self.poly_degree + 1, G2)
+
+        self.alpha_g1 = multiply(G1, self.alpha)
+        self.beta_g1 = multiply(G1, self.beta)
+        self.beta_g2 = multiply(G2, self.beta)
+
+        self.t_tau_srs = self.__build_aux_poly()
+        self.psis = self.__evaluate_qap_polys()
+    
 
     """
     Calulates the structure reference string; powers of tau in a elliptic curve group
@@ -31,40 +63,47 @@ class Setup:
     """
     def __build_aux_poly(self):
         t_xs = self.GF(np.arange(1, self.poly_degree + 1))
-        tau_GF = self.GF(self.tau)
-        t_tau_GF = np.prod(tau_GF - t_xs)
+        t_tau_GF = np.prod(self.tau_GF - t_xs)
 
         return [
-            multiply(G1, int((tau_GF**i) * t_tau_GF))
+            multiply(G1, int((self.tau_GF**i) * t_tau_GF))
             for i in range(self.poly_degree - 3, -1, -1)
         ]
 
     """
-    Takes the R1CS as a already interpolated QAP (2d array of coefficients)
-    to construct the trusted setup
+    Evaluates the QAP's polynomials at tau and constructs
+    their linear combination as corresponding G1 curve point row wise
+    G1(alpha * left_poly_i(tau) + beta * left_poly_i(tau) + out_poly_i(tau))
     """
-    def __init__(
-        self,
-        out_poly: np.ndarray, # polynomials of the output side of the qap
-        left_poly: np.ndarray, # polynomials of the left factor of the qap
-        right_poly: np.ndarray, # polynomial of the right factor of the qap
-    ):
-        ### to be kept private
-        # for powers of tau
-        self.tau = self.__get_random_scalar()
+    def __evaluate_qap_polys(self):
+        left_eval = []
+        right_eval = []
+        out_eval = []
 
-        # for multiplication with the QAP matrices
-        self.alpha = self.__get_random_scalar()
-        self.beta = self.__get_random_scalar()
-        ###
+        for i in range(self.poly_degree):
+            val_L = int(np.polyval(self.left_poly[i], self.tau)) % curve_order
+            val_R = int(np.polyval(self.right_poly[i], self.tau)) % curve_order
+            val_O = int(np.polyval(self.out_poly[i], self.tau)) % curve_order
 
-        poly_degree = len(out_poly[0])
+            left_eval.append(val_L)
+            right_eval.append(val_R)
+            out_eval.append(val_O)
 
-        self.g1_srs = self.__get_srs(poly_degree + 1, G1, self.tau)
-        self.g2_srs = self.__get_srs(poly_degree + 1, G2, self.tau)
+        
+        return [
+            multiply(
+                G1,
+                (self.alpha * right_eval[i] +
+                self.beta  * left_eval[i] +
+                out_eval[i]) % curve_order)
+                for i in range(self.poly_degree
+            )
+        ]
 
-        self.alpha_g1 = multiply(G1, self.alpha)
-        self.beta_g1 = multiply(G1, self.beta)
-        self.beta_g2 = multiply(G2, self.beta)
 
-        self.t_tau_srs = self.__build_aux_poly()
+    """
+    Returns a random interger between zero and the G1/G2-curve order (exclusive)
+    """
+    @staticmethod
+    def __get_random_scalar():
+        return random.randint(1, curve_order - 1) # Must not be 0
