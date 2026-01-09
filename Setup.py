b5d2f1d142c782f7e0edcbc032a9a2f720c26271
Fabian Strauch <fabian.strauch.04@web.de>
2026-01-09 14:35:35 +0100

    refactored setup class (yet again)

--- Setup.py
+++ Setup.py
@@ -1,11 +1,16 @@
 from py_ecc.bn128 import G1, G2, pairing, add, multiply, eq, curve_order, neg
 import numpy as np
 import random
+import galois
 
 class Setup:
+    # finite field over which G1, G2 and G12 are defined
+    GF = galois.GF(curve_order)
+
     """
     Returns a random interger between zero and the G1/G2-curve order (exclusive)
     """
+    @staticmethod
     def __get_random_scalar():
         return random.randint(1, curve_order - 1) # Must not be 0
 
@@ -14,8 +19,21 @@ class Setup:
     Needs to be passed the max polynomial degree of the QAP,
     to calculate enough powers of tau
     """
-    def __get_srs(self, poly_degree, generator, tau):
-        return [multiply(generator, tau**i) for i in range(poly_degree,-1,-1)]
+    def __get_srs(self, generator):
+        return [
+            multiply(generator, int(self.tau**i))
+            for i in range(self.poly_degree,-1,-1)
+        ]
+
+    """
+    Calculates t(tau) for the auxilary polynomial t(x) = (x-1)(x-2)...(x-n)
+    and returns the srs for it
+    """
+    def __build_aux_poly(self, tau, poly_degree):
+        t_xs = self.GF(np.arange(1,poly_degree+1))
+        self.t_tau = np.prod([(self.tau-i) % curve_order for i in t_xs]) % curve_order
+        
+        self.t_tau_srs = self.__get_srs(poly_degree)
 
     """
     Takes the R1CS as a already interpolated QAP (2d array of coefficients)
@@ -26,38 +44,21 @@ class Setup:
         out_poly: np.ndarray, # polynomials of the output side of the qap
         left_poly: np.ndarray, # polynomials of the left factor of the qap
         right_poly: np.ndarray, # polynomial of the right factor of the qap
-        witness: np.ndarray
     ):
         ### to be kept private
         # for powers of tau
-        tau = self.__get_random_scalar()
+        self.tau = self.__get_random_scalar()
 
-        # random scalars to be kept secret from the prover
-        alpha = self.__get_random_scalar()
-        beta = self.__get_random_scalar()
-
-        # divisors for the public and the private parts of the witness
-        gamma = self.__get_random_scalar()
-        delta = self.__get_random_scalar()
+        # for multiplication with the QAP matrices
+        self.alpha = self.__get_random_scalar()
+        self.beta = self.__get_random_scalar()
         ###
 
         poly_degree = len(out_poly[0])
 
-        self.g1_srs = self.__get_srs(poly_degree, G1, tau)
-        self.g2_srs = self.__get_srs(poly_degree, G2, tau)
+        self.g1_srs = self.__get_srs(poly_degree + 1, G1, self.tau)
+        self.g2_srs = self.__get_srs(poly_degree + 1, G2, self.tau)
 
-        self.alpa_g1 = multiply(alpha, G1)
-        self.beta_g1 = multiply(beta, G1)
-        self.beta_g2 = multiply(beta, G2)
-        self.gamma_g1 = multiply(gamma, G1)
-        self.delta_g1 = multiply(delta, G1)
-        self.delta_g2 = multiply(delta, G2)
-
-
-        # auxilary polynomial (t(x) = (x-1)(x-2)...(x-n))
-        # as only t(tau) is needed, it can just be calculated directly
-        # and used for the srs
-        t_xs = np.arange(1,poly_degree+1)
-        self.t_tau = np.prod([(self.tau-i)%curve_order for i in t_xs]) % curve_order
-        
-        self.t_tau_srs = self.__get_srs(poly_degree)
+        self.alpha_g1 = multiply(G1, self.alpha)
+        self.beta_g1 = multiply(G1, self.beta)
+        self.beta_g2 = multiply(G2, self.beta)
