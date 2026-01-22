cfaa72ac0c0cd3a2b42dd47defd8f4acb19d79e5
Fabian Strauch <fabian.strauch.04@web.de>
2026-01-22 13:43:11 +0100

    method for computing the h(tau)t(tau) value

--- Prover.py
+++ Prover.py
@@ -1,14 +1,18 @@
-from py_ecc.bn128 import G1, G2, multiply, curve_order, add
+from py_ecc.bn128 import multiply, curve_order, add
 import numpy as np
 
 class Prover:
     """
     Takes as input the witness to prove with,
-    aswell as the needed curve scalars and srs from the setup
+    aswell as the needed curve scalars,
+    evaluated qap polynomials and the two srs's from the setup
     """
     def __init__(
         self,
         witness: list[int],
+        left_eval,
+        right_eval,
+        out_eval,
         alpha_g1,
         beta_g2,
         g1_srs,
@@ -21,9 +25,14 @@ class Prover:
         self.g2_srs = g2_srs
         self.t_tau_srs = t_tau_srs
         self.psis = psis
+        self.left_eval = left_eval
+        self.right_eval = right_eval
+        self.out_eval = out_eval
 
         self.a_1 = self.__compute_AB_point(alpha_g1, g1_srs)
         self.a_1 = self.__compute_AB_point(beta_g2, g2_srs)
+
+        self.h = self.__compute_h_tau()
 
     """
     Can construct both the [A]_1 point (with alpha_g1 and srs_g1)
@@ -38,4 +47,15 @@ class Prover:
 
         return acc
     
-    def compute_C_point(self):
+    """
+    Calculates the value of h(tau)t(tau)
+    (as h = .../t, t_srs is not needed here, as it disapears)
+    """
+    def __compute_h_tau(self):
+        sum_l = sum(int(a) * int(l_val) for a, l_val in zip(self.witness, self.left_eval))
+        sum_r = sum(int(a) * int(r_val) for a, r_val in zip(self.witness, self.right_eval))
+        sum_o = sum(int(a) * int(o_val) for a, o_val in zip(self.witness, self.out_eval))
+
+        res = (sum_l * sum_r - sum_o) % curve_order
+
+        return res
