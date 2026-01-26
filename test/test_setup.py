adbf64d3b607fa4f2ca4f9436a8ba772bafe2f9c
Fabian Strauch <fabian.strauch.04@web.de>
2026-01-26 19:57:26 +0100

    simple init test of setup

--- test/test_setup.py
+++ test/test_setup.py
@@ -4,6 +4,15 @@ from py_ecc.bn128 import curve_order
 import pickle
 from utils import project_path
 
+# for class import from parent dir (god forbid I could just use a file path for imports...)
+import sys
+import os
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+sys.path.append(parent_dir)
+
+
+from Setup import Setup
 
 # must be initalized before pickle data import
 GF = galois.GF(curve_order)
@@ -11,27 +20,20 @@ GF = galois.GF(curve_order)
 with open(project_path("test", "qap_data.pkl"), "rb") as f:
     data = pickle.load(f)
 
-def main():
-    qap = data
+setup = Setup(
+    out_polys=data["out_polys"],
+    left_polys=data["left_polys"],
+    right_polys=data["right_polys"]
+)
 
-    setup = Setup(
-        out_polys=qap["out_polys"],
-        left_polys=qap["left_polys"],
-        right_polys=qap["right_polys"]
-    )
+trusted_setup = setup.get_setup()
 
-    data = setup.get_setup()
+print("Setup Output erhalten")
 
-    print("Trusted setup completed")
-    print(f"Curve order: {curve_order}")
-    print(f"Random tau: {setup.tau}")
-    print(f"Random alpha: {setup.alpha}")
-    print(f"Random beta: {setup.beta}")
-
-    print("\nSRS G1 length:", len(data["g1_srs"]))
-    print("SRS G2 length:", len(data["g2_srs"]))
-    print("t(tau) SRS length:", len(data["t_tau_srs"]))
-    print("Psi points:", len(data["psis"]))
-
-if __name__ == "__main__":
-    main()
+print(f"Setup erfolgreich erstellt!")
+print(f"Anzahl Polynome: {setup.num_polys}")
+print(f"Anzahl Constraints: {setup.num_constraints}")
+print(f"Länge G1 SRS: {len(trusted_setup['g1_srs'])}")
+print(f"Länge G2 SRS: {len(trusted_setup['g2_srs'])}")
+print(f"Länge t(τ) SRS: {len(trusted_setup['t_tau_srs'])}")
+print(f"Anzahl Ψ Elemente: {len(trusted_setup['psis'])}")
